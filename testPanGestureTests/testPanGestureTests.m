619b4c80b5a7f5cd3b53f4f8065174270e4e9ca3
Matthew Surowiec <mp5auc3@gmail.com>
2014-07-28 23:08:20 -0400

    Should work

---
+++ testPanGestureTests/testPanGestureTests.m
@@ -0,0 +1,34 @@
+//
+//  testPanGestureTests.m
+//  testPanGestureTests
+//
+//  Created by matt on 7/22/14.
+//  Copyright (c) 2014 mpsauce. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface testPanGestureTests : XCTestCase
+
+@end
+
+@implementation testPanGestureTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
